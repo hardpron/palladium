b29d77f912015da7a20a07f1686839dadb073140
Flamine <flamine@list.ru>
2015-02-04 00:06:52 +0300

    add new model - product. Add controller, pages for create, destroy, edit and show this resource.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130162525) do
+ActiveRecord::Schema.define(version: 20150203193654) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "status"
+    t.string   "version"
+    t.string   "update_data"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "test_cases", force: :cascade do |t|
     t.string   "title"
