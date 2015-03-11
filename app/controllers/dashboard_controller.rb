60b7c8c526edbdbbccc5936f86066d67bb85b12e
flamine <flamine@list.ru>
2015-03-11 19:50:23 +0300

    comment listener action and edit rotes file

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,27 +3,27 @@ class DashboardController < ApplicationController
   def index
   end
 
-  def listener
-    params.merge!({'update_data' => Time.now.to_s})
-    p params
-    case params['act']
-      when 'create_product'
-        Product.create('name' => params['name'], 'status' => params['status'], 'version' => params['version'], 'update_data' => params['update_data'])
-      when 'delete_all_products'
-        Product.destroy_all
-      when 'create_run_for_product'
-       Product.find_by_name(params['product_name']).runs << Run.create('date' => params['update_data'], 'version' => params['version'], 'status' => params['run_status'], )
-      when 'delete_all_runs'
-        Run.destroy_all
-    end
-    puts '
-    ████████████████████████████████████████████████████
-    █────██────██─████─████────██────███───██─█─██─███──█
-    █─██─██─██─██─████─████─██─██─██──███─███─█─██──█───█
-    █────██────██─████─████────██─██──███─███─█─██─█─█──█
-    █─█████─██─██─████─████─██─██─██──███─███─█─██─███──█
-    █─█████─██─██───██───██─██─██────███───██───██─███──█
-    ████████████████████████████████████████████████████'
-    render :something
-  end
+  # def listener
+  #   params.merge!({'update_data' => Time.now.to_s})
+  #   p params
+  #   case params['act']
+  #     when 'create_product'
+  #       Product.create('name' => params['name'], 'status' => params['status'], 'version' => params['version'], 'update_data' => params['update_data'])
+  #     when 'delete_all_products'
+  #       Product.destroy_all
+  #     when 'create_run_for_product'
+  #      Product.find_by_name(params['product_name']).runs << Run.create('date' => params['update_data'], 'version' => params['version'], 'status' => params['run_status'], )
+  #     when 'delete_all_runs'
+  #       Run.destroy_all
+  #   end
+  #   puts '
+  #   ████████████████████████████████████████████████████
+  #   █────██────██─████─████────██────███───██─█─██─███──█
+  #   █─██─██─██─██─████─████─██─██─██──███─███─█─██──█───█
+  #   █────██────██─████─████────██─██──███─███─█─██─█─█──█
+  #   █─█████─██─██─████─████─██─██─██──███─███─█─██─███──█
+  #   █─█████─██─██───██───██─██─██────███───██───██─███──█
+  #   ████████████████████████████████████████████████████'
+  #   render :something
+  # end
 end
