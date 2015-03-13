6016019ab8ba53d247381fc39159394f6b674dd3
flamine <flamine@list.ru>
2015-03-14 01:09:46 +0300

    add new styles for dashbord page, remode 'show', 'edit' and 'delete' button from dashbord page, delete listener action

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,28 +2,4 @@ class DashboardController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:ipn_notification]
   def index
   end
-
-  # def listener
-  #   params.merge!({'update_data' => Time.now.to_s})
-  #   p params
-  #   case params['act']
-  #     when 'create_product'
-  #       Product.create('name' => params['name'], 'status' => params['status'], 'version' => params['version'], 'update_data' => params['update_data'])
-  #     when 'delete_all_products'
-  #       Product.destroy_all
-  #     when 'create_run_for_product'
-  #      Product.find_by_name(params['product_name']).runs << Run.create('date' => params['update_data'], 'version' => params['version'], 'status' => params['run_status'], )
-  #     when 'delete_all_runs'
-  #       Run.destroy_all
-  #   end
-  #   puts '
-  #   ████████████████████████████████████████████████████
-  #   █────██────██─████─████────██────███───██─█─██─███──█
-  #   █─██─██─██─██─████─████─██─██─██──███─███─█─██──█───█
-  #   █────██────██─████─████────██─██──███─███─█─██─█─█──█
-  #   █─█████─██─██─████─████─██─██─██──███─███─█─██─███──█
-  #   █─█████─██─██───██───██─██─██────███───██───██─███──█
-  #   ████████████████████████████████████████████████████'
-  #   render :something
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :products
   get 'products/index'
-
+  post '/products/new' #button 'create new product' dont work if delete if
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
