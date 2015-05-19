eb960bb67d45c12852d895a5a60203d5a6c39e71
flaminestone <flamine@list.ru>
2015-05-19 13:57:37 +0300

    add new resource "Status"

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :id, :name, :colore, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514083727) do
+ActiveRecord::Schema.define(version: 20150519094820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 20150514083727) do
     t.string   "name"
     t.string   "version"
     t.integer  "plan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "colore"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
