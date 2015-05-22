b228b74becb0ca03e0126a75bf63cb52a1a0250b
flaminestone <flamine@list.ru>
2015-05-22 13:53:09 +0300

    change application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require highcharts
-// highcharts and plugin/exporting.js need for standats charts, highcharts-3d need for 3d charts
-//= require highcharts-3d
 //= require plugin/exporting.js
+//= require highcharts-3d
+// highcharts and plugin/exporting.js need for standats charts, highcharts-3d need for 3d charts #succession!!!#
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
