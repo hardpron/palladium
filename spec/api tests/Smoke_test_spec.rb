d491b434318cd067b1eeca77e9e778e3cb3eabca
flaminestone <flamine@list.ru>
2015-06-01 15:30:24 +0300

    edit spec file/ Delete creating new product - move it to before :all

--- spec/api tests/Smoke_test_spec.rb
+++ spec/api tests/Smoke_test_spec.rb
@@ -4,7 +4,17 @@ describe 'Products' do
 
   before :all do
     @api = Api.new('localhost:3000', 'flamine@list.ru', '4s8Fq325PJmsD1frVSHx')
-    @api.add_new_product({:product => {:name => 'name', :status => 'status', :version => 'version'}})
+    @product_name = "name#{Time.now.nsec}"
+    @product_status = "status#{Time.now.nsec}"
+    @product_version = "version#{Time.now.nsec}"
+    @api.add_new_product({:product => {:name => @product_name, :status => @product_status, :version => @product_version}})
+  end
+
+  it 'add_new_product' do
+    params = {:product => {:name => "name#{Time.now.nsec}", :status => "status#{Time.now.nsec}", :version => "version#{Time.now.nsec}"}}
+    @api.add_new_product(params)
+    response = JSON.parse(@api.get_products_by_param({:name => params[:product][:name]}))
+    expect(response.values.size).to eq 1
   end
 
   it 'get_all_products' do
@@ -13,15 +23,7 @@ describe 'Products' do
   end
 
   it 'get_products_by_param' do
-    name = "name#{Time.now.nsec}"
-    params = {:product => {:name => name, :status => 'status', :version => 'version'}}
-    @api.add_new_product(params)
-    response = JSON.parse(@api.get_products_by_param({:name => name}))
-    expect(response.values.first['ProductName']).to eq(name)
-  end
-
-  it 'add_new_product' do
-    params = {:product => {:name => 'name', :status => 'status', :version => 'version'}}
-    expect(@api.add_new_product(params).code).to eq('302')
+    response = JSON.parse(@api.get_products_by_param({:name => @product_name}))
+    expect(response.values.first['ProductName']).to eq(@product_name)
   end
 end
