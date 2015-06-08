d30dedd3ee7f183addef4234ac65420744a7f4da
flaminestone <flamine@list.ru>
2015-06-08 16:34:59 +0300

    delete not use method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,19 +10,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     products_path
   end
-
-  protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      if request.post?
-
-      else
-        redirect_to login_path, :notice => 'if you want to add a notice'
-      end
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
 end
