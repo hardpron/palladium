d2390ed164b98011cd98a5dd9fbfbf76d0fdacdc
flaminestone <flamine@list.ru>
2015-06-15 14:42:38 +0300

    edit chart data by standart

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,11 @@
 class Result < ActiveRecord::Base
   belongs_to :result_set
   belongs_to :status
+  after_commit :count_result_set_status
+
+  def count_result_set_status
+    unless self.status.nil? || self.result_set_id.nil?
+      ResultSet.find(self.result_set_id).status
+    end
+  end
 end
