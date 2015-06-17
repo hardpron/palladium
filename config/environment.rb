393fe7a0c571d5551300fead6d77255714df805c
flaminestone <flamine@list.ru>
2015-06-17 10:17:13 +0300

    edit Gemfile: add 'pg' gem to all groups, comment # Status.create(name:"Untested", color:"#FFFFFF", main_status:true) if Status.find_by_main_status(true).nil? because db cant start after drop db

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Status.create(name:"Untested", color:"#FFFFFF", main_status:true) if Status.find_by_main_status(true).nil?
+# Status.create(name:"Untested", color:"#FFFFFF", main_status:true) if Status.find_by_main_status(true).nil?
 
