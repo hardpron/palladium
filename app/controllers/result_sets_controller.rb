10b291e1f916e048a7f098b292745a366526eef8
flaminestone <flamine@list.ru>
2015-06-26 10:19:19 +0300

    add statistic page

--- app/controllers/result_sets_controller.rb
+++ app/controllers/result_sets_controller.rb
@@ -13,6 +13,10 @@ class ResultSetsController < ApplicationController
   # GET /result_sets/1
   # GET /result_sets/1.json
   def show
+    product = Product.find(product_find_by_id)
+    @result_set = set_result_set
+    @run = set_run
+    @results = Result.where(result_set_id: ResultSet.where(run_id: Run.where(plan_id: product.plans.ids).ids, name: @result_set.name).ids)
   end
 
   # GET /result_sets/new
@@ -81,15 +85,15 @@ class ResultSetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_result_set
-      @result_set = ResultSet.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_result_set
+    @result_set = ResultSet.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def result_set_params
-      params.require(:result_set).permit(:name, :date, :version)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def result_set_params
+    params.require(:result_set).permit(:name, :date, :version)
+  end
 
   def set_run
     @run = Run.find(params[:run_id])
@@ -108,14 +112,14 @@ class ResultSetsController < ApplicationController
     result_sets_json = {}
     ResultSet.all.each do |current_result_set|
       result_sets_json.merge!(current_result_set.id => {'name' => current_result_set.name,
-                                                         'date' => current_result_set.date,
-                                                         'version' => current_result_set.version,
-                                                         'run_id' => current_result_set.run_id,
-                                                         'created_at' => current_result_set.created_at,
-                                                         'updated_at' => current_result_set.updated_at})
+                                                        'date' => current_result_set.date,
+                                                        'version' => current_result_set.version,
+                                                        'run_id' => current_result_set.run_id,
+                                                        'created_at' => current_result_set.created_at,
+                                                        'updated_at' => current_result_set.updated_at})
     end
     render :json => result_sets_json
-    end
+  end
 
   def get_result_sets_by_param
     result_sets_json = {}
@@ -124,14 +128,15 @@ class ResultSetsController < ApplicationController
     if result.empty?
       render :json => {}
     else
-      result = [result] if result.count == 1
-      result.map do |current_result|
+      result = [result] until result.is_a?(Array)
+      result.each do |current_result|
         result_sets_json.merge!(current_result.id => {'name' => current_result.name,
-                                                           'date' => current_result.date,
-                                                           'version' => current_result.version,
-                                                           'run_id' => current_result.run_id,
-                                                           'created_at' => current_result.created_at,
-                                                           'updated_at' => current_result.updated_at})
+                                                      'date' => current_result.date,
+                                                      'version' => current_result.version,
+                                                      'status' => current_result.status,
+                                                      'run_id' => current_result.run_id,
+                                                      'created_at' => current_result.created_at,
+                                                      'updated_at' => current_result.updated_at})
       end
       render :json => result_sets_json
     end
