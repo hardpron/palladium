d4095a5f5036149e607f10a2e496f38d3a41eb4a
flaminestone <flamine@list.ru>
2015-07-02 16:42:37 +0300

    delete status and version fields for product and add product smoke spec

--- spec/Example/APIShell.rb
+++ spec/Example/APIShell.rb
@@ -4,7 +4,7 @@ class APIShell
   attr_accessor :add_all_suites, :ignore_parameters, :suites_to_add, :search_plan_by_substring, :in_debug
 
   def initialize(product_name, plan_name, run_name)
-    @api = Api.new('localhost:3000', 'flamine@list.ru', '6gVmTd6Xk79z-4TwibSu')
+    @api = Api.new('localhost:3000', 'flamine@list.ru', 'NX_FY9zLz5BFH-qVvDHE')
     # @in_debug = RspecHelper.debug?
     # if @in_debug
     #   LoggerHelper.print_to_log 'Do not initialize Palladium, because spec run in debug'
--- spec/Example/Multiplication_spec.rb
+++ spec/Example/Multiplication_spec.rb
@@ -6,34 +6,14 @@ data = nil
 set_name = ''
 plan_data = ''
 describe 'Multiplication' do
-  # testrail = TestrailHelper.new SettingsEditors::DOCUMENT::PROJECT_NAME, name_of_suite, starter.plan_name do |testrail_config|
-  #   testrail_config.ignore_parameters = starter.ignored_tags
-  # end
-  palladium = APIShell.new 'Multiplication', 'plan_3', "Multiplication Tests"
+  palladium = APIShell.new 'CDE', 'plan_1', "Multiplication Tests_15"
   describe 'Correct' do
-    (1..100).to_a.each do |current_element_first|
-      [1,2,3,4,5,6,7,8,9].each do |current_element_second|
-        it "#{current_element_first}*#{current_element_second}" do
-          set_name = "#{current_element_first}*#{current_element_second} Correct"
-          result = current_element_first*current_element_second
-          expect(DataHelper.get_result_by_params(current_element_first, current_element_second)).to eq(result)
+      (1..200).to_a.each do |current_element_second|
+        it "#{current_element_second}" do
+          expect(current_element_second*2).to eq(current_element_second*2)
         end
       end
-    end
   end
-
-  # describe 'Uncorrect' do
-  #   [1,2,3,4,5,6,7,8,9].each do |current_element_first|
-  #     [1,2,3,4,5,6,7,8,9].each do |current_element_second|
-  #       it "#{current_element_first}*#{current_element_second}" do
-  #         set_name = "#{current_element_first}*#{current_element_second} Uncorrect"
-  #         run_name = 'Uncorrect'
-  #         result = current_element_first*current_element_second
-  #         expect(0).to eq(result)
-  #       end
-  #     end
-  #   end
-  # end
 
   after :each do |example|
     palladium.add_result(example)
