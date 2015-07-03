c3fefcc05a8a5095e6ccc4f5bbd3704b7ce2c2e6
flaminestone <flamine@list.ru>
2015-07-03 17:32:38 +0300

    edit multi tests

--- spec/Example/Multiplication_spec.rb
+++ spec/Example/Multiplication_spec.rb
@@ -1,17 +1,23 @@
 require_relative 'DataHelper'
 require_relative 'APIShell'
-
+@palladium = nil
 describe 'Multiplication' do
-  palladium = APIShell.new 'CDE', 'plan_4', "Multiplication Tests_18"
+  (1..1).to_a.each do |i|
+
+
   describe 'Correct' do
-      (1..100).to_a.each do |current_element_second|
-        it "#{current_element_second}" do
-          expect(current_element_second*2).to eq(current_element_second*2)
+    (1..800).to_a.each do |current_element_second|
+      it "#{current_element_second}" do
+        @palladium = APIShell.new 'CDE', 'plan_3', "Multiplication Tests_#{i}"
+        expect(current_element_second*2).to eq(current_element_second*2)
         end
       end
+      after :each do |example|
+        @palladium.add_result(example)
+      end
+  end
   end
 
-  after :each do |example|
-    palladium.add_result(example)
-  end
+
+
 end
